aba2ddcdcfc8f436f4f720bcafafb5abb92112d3
DrizzyJ56 <blevins.j921@gmail.com>
2023-03-29 15:59:53 -0400

    working on seed

---
+++ db/users.js
@@ -0,0 +1,53 @@
+const { client } = require(".")
+
+async function createUser({username, password, email, first_name, last_name}){
+    try {
+        const {rows: [user]} = await client.query(`
+            INSERT INTO users (username, password, email, first_name, last_name)
+            VALUES ($1, $2, $3, $4, $5)
+            ON CONFLICT (username) DO NOTHING
+            RETURNING *
+        `, [username, password, email, first_name, last_name])
+    
+        return user
+    } catch (error) {
+        throw error
+    }
+}
+
+async function getUserById(userId){
+    try {
+        const {rows: [user]} = await client.query(`
+            SELECT * FROM users
+            WHERE id=$1;
+        `, [userId])
+        if(user){
+            delete user.password
+        }
+        return user
+    } catch (error) {
+        throw error 
+    }
+}
+
+async function getAllUsers(){
+    try {
+        const {rows} = await client.query(`
+            SELECT id, username, first_name, last_name, email, is_admin, active 
+            FROM users;
+        `)
+        return rows
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {
+    createUser,
+    getUserById,
+    getAllUsers,
+}
+
+
+
+
