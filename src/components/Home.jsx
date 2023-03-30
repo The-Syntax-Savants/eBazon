7efcff98206558a0e7d489d596ff7c29e642fa5e
Charles Rooney <crooney36@gmail.com>
2023-03-30 15:44:02 -0400

    single product car

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { SingleProductCard } from "./";
+require("../style.css");
+require("../tailwind.config.js");
+
+const Home = () => {
+  return <></>;
+};
+
+export default Home;
