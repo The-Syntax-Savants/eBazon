dacdec9632e591311c7eb65ba842abb1959dfa39
DrizzyJ56 <blevins.j921@gmail.com>
2023-03-30 10:19:10 -0400

    finished index.js middleware and basic seed

---
+++ api/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const apiRouter = express.Router()
+const jwt = require("jsonwebtoken")
+const {getUserById} = require("../db/users")
+const {JWT_SECRET} = process.env
+
+apiRouter.get("/health", async (req, res, next) => {
+    try {
+      res.send({ message: "Server is healthy" });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+apiRouter.use(async (req,res,next) => {
+    const prefix = "Bearer "
+    const auth = req.header("Authorization")
+    
+    if(!auth){
+        next()
+    }else if(auth.startsWith(prefix)){
+        const token = auth.slice(prefix.length)
+        try {
+            const {id} = jwt.verify(token, JWT_SECRET)
+            if(id){
+                req.user = await getUserById()
+                if(req.user){
+                    console.log("User is set:", req.user)
+                }
+                next()
+            }
+        } catch ({name, message}) {
+            next({name, message})
+        }
+    }else{
+        next({
+            name: "AuthorizationHeaderError",
+            message: `Authorization token must start with ${prefix}`
+        })
+    }
+})
+
+
+
+
+const usersRouter = require("./users");
+apiRouter.use("/users", usersRouter);
+
+const productsRouter = require("./products")
+apiRouter.use("/products", productsRouter)
+
+const cartsRouter = require("./carts")
+apiRouter.use("/carts", cartsRouter)
+
+const tags = require("./tags");
+apiRouter.use("/tags", tags);
+
+module.exports = apiRouter
