9e2cc2d1994c0b21b89568706d348e3fa72daa3d
DrizzyJ56 <blevins.j921@gmail.com>
2023-04-07 14:49:21 -0400

    ready to test deploy

--- src/api-adapters/admin.js
+++ src/api-adapters/admin.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3001/api";
-// const BASE_URL = "https://ebazon.onrender.com/api"
+// const BASE_URL = "http://localhost:3001/api";
+const BASE_URL = "https://ebazon.onrender.com/api"
 
 export const deleteProductInDB = async (id, token) => {
     try {
--- src/api-adapters/tags.js
+++ src/api-adapters/tags.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:3001/api";
+// const BASE_URL = "http://localhost:3001/api";
+const BASE_URL = "https://ebazon.onrender.com/api"
 
 export const getAllTagsDB = async () => {
   try {
