3ae3b3f88fc6706eff9078dd97c38d8d020b0f82
PolyNoodle94 <emidollar@hotmail.com>
2023-04-08 00:01:57 -0500

    Added bottom margin to navbar-container div. Added flex flex-wrap to product container that way our products dont _stretch_ the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,7 +60,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div id="product-cards-container">
+      <div id="product-cards-container" className="flex flex-wrap">
         {products.map((product) => {
           return (
             <SingleProductCard
