978cadc3a6ca3ad02c5d417839c7ebe3381086fb
PolyNoodle94 <emidollar@hotmail.com>
2023-04-10 09:26:00 -0500

    Got the styling of the search bar looking exactly as we wanted it. Working on adding the dropdown functionality of the tag search input. Working on Submit button for the form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -134,32 +134,15 @@ const Navbar = (props) => {
       </div>
       <div className="navbar-end">
 
-        {/* --->>>>> Search Button */}
-
-        {/* <button className="btn btn-ghost btn-circle border-dashed border-2 border-indigo-600">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button> */}
-
-        <form action="" ref={ refOne } className="relative w-max flex flex-row border-dashed border-2 border-indigo-600">
-          <input type="search" value="Search Products" name="search" id="search" 
+        <form action="" ref={ refOne } className="relative w-max flex flex-row ">
+          {/* THIS IS THE SEARCH INPUT */}
+          <input type="search" placeholder="Search Products" name="search" id="search" 
             className="relative peer z-10 bg-transparent w-12 h-12 rounded-full border cursor-pointer outline-none
             pl-12 
             focus:w-full focus:border-indigo-600 focus:cursor-text focus:pl-16 focus:pr-4" 
           />
 
+          {/* THIS IS THE SEARCH SVG*/}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="absolute inset-y-0 my-auto h-8 w-12 px-3.5 stroke-gray-500 border-r border-transparent peer-focus:border-indigo-600 peer-focus:stroke-indigo-600"
@@ -175,24 +158,31 @@ const Navbar = (props) => {
             />
           </svg>
 
-          <input type="search"  value="Search Tags"  name="search-tag" id="search-tag" 
-            className={`relative peer z-10 bg-transparent w-12 h-12 rounded-full border cursor-pointer outline-none
-            pl-12 
-            focus:w-full focus:border-indigo-600 focus:cursor-text focus:pl-16 focus:pr-4 ${visible}`} 
-          />
+          {/* THIS IS THE TAGS INPUT */}
 
-        <svg xmlns="http://www.w3.org/2000/svg" 
-          className={`absolute inset-y-0 my-auto h-8 w-12 px-3.5 stroke-gray-500 border-r border-transparent peer-focus:border-indigo-600 peer-focus: stroke-indigo-600 ${visible}`} fill="none" 
-          viewBox="0 0 24 24" 
-          stroke="currentColor" 
-          strokeWidth={2}
-        >
-          <path 
-            strokeLinecap="round" 
-            strokeLinejoin="round" 
-            d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" 
+          <div className="relative ml-2">
+
+            <input type="search"  placeholder="Search Tags"  name="search-tag" id="search-tag" 
+              className={`relative peer z-10 bg-transparent w-12 h-12 rounded-full border cursor-pointer outline-none pl-12 focus:w-full focus:border-indigo-600 focus:cursor-text focus:pl-16 focus:pr-4 ${visible}`} 
             />
-        </svg>
+
+
+            {/* THIS IS THE TAGS SVG */}
+
+            <svg xmlns="http://www.w3.org/2000/svg" 
+              className={`absolute inset-y-0 my-auto h-8 w-12 px-3.5 stroke-gray-500 border-r border-transparent peer-focus:border-indigo-600 peer-focus: stroke-indigo-600  ${visible}`} fill="none" 
+              viewBox="0 0 24 24" 
+              stroke="currentColor" 
+              strokeWidth={2}
+            >
+              <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" 
+                />
+            </svg>
+
+          </div>
 
 
 
