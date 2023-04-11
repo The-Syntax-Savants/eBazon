b664ac4204537a7f44e383350869c0ed62429cbe
Charles Rooney <crooney36@gmail.com>
2023-04-11 10:39:06 -0400

    AWS cognito for unauth users to S3

--- aws.config.js
+++ aws.config.js
@@ -1,7 +1,14 @@
 import AWS from "aws-sdk";
 
+const IDENTITY_POOL_ID = "us-east-1:71d2ff86-b029-4567-8d5b-51d797f61400";
+const REGION = "us-east-1";
+
+// Configure the AWS SDK with Cognito identity credentials
 AWS.config.update({
-  region: "us-east-1",
+  region: REGION,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IDENTITY_POOL_ID,
+  }),
 });
 
 const s3 = new AWS.S3();
