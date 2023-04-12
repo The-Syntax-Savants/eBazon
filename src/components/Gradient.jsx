4d8c416a03eee35d6d4cb404f3848f0a9a2f951b
Charles Rooney <crooney36@gmail.com>
2023-04-12 15:17:43 -0400

    new test branch

---
+++ src/components/Gradient.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Gradient = () => {
+  return (
+    <>
+      <span id="gradient" />
+      <style>{`
+        #teoti-gradient {
+          position: relative;
+          display: flex;
+          width: 100%;
+          height: 8px !important;
+          background: linear-gradient(
+            90deg,
+            #3f37c9 0%,
+            #3a0ca3 22.4%,
+            #560bad 42.71%,
+            #7209b7 61.98%,
+            #b5179e 81.77%,
+            #f72585 100%
+          );
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default Gradient;
