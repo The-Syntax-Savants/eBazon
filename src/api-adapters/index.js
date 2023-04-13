d461bb276a697baba711ba08a7454085fb12f8e7
Charles Rooney <crooney36@gmail.com>
2023-04-13 13:53:23 -0400

    cart fix

--- src/api-adapters/index.js
+++ src/api-adapters/index.js
@@ -2,7 +2,7 @@
 // Use the production API URL when the environment is "production"
 // Use the localhost API URL for development purposes
 
-export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://ebazon.onrender.com/api"
-    : "http://localhost:3001/api";
+export const BASE_URL = "https://ebazon.onrender.com/api";
+// process.env.NODE_ENV === "production"
+//   ? "https://ebazon.onrender.com/api"
+//   : "http://localhost:3001/api";
