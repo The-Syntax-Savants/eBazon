698af0e5f3e27f995f1e5f0c3ecd45a797ead8f3
DrizzyJ56 <blevins.j921@gmail.com>
2023-04-20 09:36:18 -0400

    cleaned up some console logs in messages/api and fixed routes so logged in users can't see other users messages

--- api/messages.js
+++ api/messages.js
@@ -1,57 +1,80 @@
 import express from "express";
 import { requireUser } from "./utils.js";
-import { createMessage, getAllUnreadMessagesByUsername } from "../db/messages.js";
+import {
+  createMessage,
+  createOffer,
+  getAllUnreadMessagesByUsername,
+} from "../db/messages.js";
 
-export const messagesRouter = express.Router()
-export default messagesRouter
+export const messagesRouter = express.Router();
+export default messagesRouter;
 
 messagesRouter.use((req, res, next) => {
-    console.log("A request is being made to /messages");
-  
-    next();
-  });
+  console.log("A request is being made to /messages");
 
-  //Create a new message
-  messagesRouter.post("/:productId", requireUser, async(req,res,next)=> {
-    try {
-        const {productId} = req.params
-        const info = req.body
-        info.productId = Number(productId)
-        if(info.senderName){
-            console.log(info, "THIS IS INFO")
-            const newMessage = await createMessage(info)
-            if(newMessage.receiver_name){
-                res.send(newMessage)
-            }else{
-                next({
-                    name: "createMessageError",
-                    message: "There was an error creating a message"
-                })
-            }
-        }
-        
-    } catch ({name, message}) {
-        next({name, message})
-    }
-    
-  })
+  next();
+});
 
-  //Get All Unread Messages By Username
-  messagesRouter.get("/:username", requireUser, async(req,res,next)=> {
-    try {
-        const {username} = req.params
-        const messages = await getAllUnreadMessagesByUsername(username)
-        if(messages.length){
-            res.send(messages)
-        }else if(messages.length === 0 && !messages.name){
-            res.send("This user has no messages")
-        }else{
-            next({
-                name: "UserDoesNotExistError",
-                message: "This user does not exist"
-            })
-        }
-    } catch ({name, message}) {
-        next({name, message})
+//Create a new message
+messagesRouter.post("/:productId", requireUser, async (req, res, next) => {
+  try {
+    const { productId } = req.params;
+    const info = req.body;
+    info.productId = Number(productId);
+    if (!info.offerPrice && req.user.username === info.senderName) {
+      const newMessage = await createMessage(info);
+      if (newMessage.receiver_name) {
+        res.send(newMessage);
+      } else {
+        next({
+          name: "createMessageError",
+          message: "There was an error creating a message",
+        });
+      }
+    } else if (info.offerPrice && req.user.username === info.senderName) {
+      const newOffer = await createOffer(info);
+      if (newOffer.offer_price) {
+        res.send(newOffer);
+      } else {
+        next({
+          name: "createOfferError",
+          message: "There was an error creating an offer",
+        });
+      }
+    }else{
+        next({
+            name: "userMustBeLoggedInError",
+            message: "You are either not logged in, or trying to send a message as a user that is not you"
+        })
     }
-  })
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+//Get All Unread Messages By Username
+messagesRouter.get("/:username", requireUser, async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    if(req.user.username === username || req.user.is_admin){
+        const messages = await getAllUnreadMessagesByUsername(username);
+        if (messages.length) {
+          res.send(messages);
+        } else if (messages.length === 0 && !messages.name) {
+          res.send("This user has no messages");
+        } else {
+          next({
+            name: "UserDoesNotExistError",
+            message: "This user does not exist",
+          });
+        }
+    }else{
+        next({
+            name: "UserMustBeLoggedInError",
+            message: "You are either not logged in, or trying to get somebody's messages who is not you."
+        })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
